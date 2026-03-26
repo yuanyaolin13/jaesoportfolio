ff935500cfd4eca5757568dc6054c4f836e2f4dd
Yuanyao Lin <71469791+yuanyaolin13@users.noreply.github.com>
2026-03-26 19:57:03 -0400

    test

--- src/components/pages/Project2.tsx
+++
@@ -1,7 +0,0 @@
-function Project2() {
-  return (
-    <div>Project 2 Page</div>
-  );
-}
-
-export default Project2;
--- src/components/pages/Project3.tsx
+++
@@ -1,7 +0,0 @@
-function Project3() {
-  return (
-    <div>Project 3 Page</div>
-  );
-}
-
-export default Project3;
